1c538fd66045e6f2e2028805d61e730089001116
BrandonJack9 <bjack@ucsc.edu>
2023-10-12 19:13:48 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Brandon's Game";
+const buttonName = "🍆";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("Button");
+button.innerHTML = buttonName;
+app.append(button);
