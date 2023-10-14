7a5a733e12acda2131066ba49b09b014aa57fcc0
BrandonJack9 <bjack@ucsc.edu>
2023-10-13 21:39:48 -0700

    step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 let counter: number = 0;
-const growthRate: number = 1;
+let growthRate: number = 0;
 
-const gameName = "Brandon's Game";
-const buttonName = "🍆";
+const gameName = "Satan's Army";
+const incrementDemons = "😈";
+const honorGuard = "👹";
 
 document.title = gameName;
 
@@ -13,26 +14,42 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("Button");
-button.innerHTML = buttonName;
-button.addEventListener("click", () => {
+const startButton = document.createElement("Button");
+startButton.innerHTML = incrementDemons;
+startButton.addEventListener("click", () => {
   counter++;
+  log.innerHTML = `Number of warriors: ${counter.toFixed(2)}`;
+  requestAnimationFrame(tick);
 });
-app.append(button);
+app.append(startButton);
+
+const honorGuardButton = document.createElement("button");
+honorGuardButton.innerHTML = honorGuard;
+honorGuardButton.disabled = true;
+
+honorGuardButton.addEventListener("click", () => {
+  growthRate++;
+  if (counter >= 10) {
+    counter -= 10;
+    log.innerHTML = `Number of warriors: ${counter.toFixed(2)}`;
+  }
+});
+
+app.append(honorGuardButton);
 
 let lastMillis = 0;
 
 const log = document.createElement("p");
-log.innerHTML = `No fruit!`;
+log.innerHTML = `No Warriors!`;
 app.append(log);
 
 function tick(millis: number) {
   const delta = millis - lastMillis;
   lastMillis = millis;
-
+  if (counter >= 10) {
+    honorGuardButton.disabled = false;
+  } else honorGuardButton.disabled = true;
   counter += (growthRate * delta) / 1000;
-  log.innerHTML = `Number of fruit: ${counter.toFixed(2)}`;
+  log.innerHTML = `Number of warriors: ${counter.toFixed(2)}`;
   requestAnimationFrame(tick);
 }
-
-requestAnimationFrame(tick);
