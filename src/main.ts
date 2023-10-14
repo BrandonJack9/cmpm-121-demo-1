04a78da053e5dff4f9eb5e66c9f52ca5e9c69473
BrandonJack9 <bjack@ucsc.edu>
2023-10-14 04:55:22 -0700

    step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ let growthRate: number = 0;
 const gameName = "Satan's Army";
 const incrementDemons = "😈";
 const honorGuard = "👹";
+const skeleton = "💀";
 
 document.title = gameName;
 
@@ -23,12 +24,16 @@ startButton.addEventListener("click", () => {
 });
 app.append(startButton);
 
+app.append(document.createElement("div"));
+
+let gremlins = 0;
 const honorGuardButton = document.createElement("button");
-honorGuardButton.innerHTML = honorGuard;
+honorGuardButton.innerHTML = `<font size = "+2">👹</font><br>Gremlins: ${gremlins}`;
 honorGuardButton.disabled = true;
 
 honorGuardButton.addEventListener("click", () => {
-  growthRate++;
+  gremlins++;
+  growthRate += 0.1;
   if (counter >= 10) {
     counter -= 10;
     log.innerHTML = `Number of warriors: ${counter.toFixed(2)}`;
@@ -37,11 +42,46 @@ honorGuardButton.addEventListener("click", () => {
 
 app.append(honorGuardButton);
 
+let goblins = 0;
+const goblinButton = document.createElement("button");
+goblinButton.disabled = true;
+goblinButton.innerHTML = `<font size = "+2">👺</font><br>Goblins: ${goblins}`;
+
+goblinButton.addEventListener("click", () => {
+  goblins++;
+  growthRate += 2;
+  if (counter >= 100) {
+    counter -= 100;
+    log.innerHTML = `Number of warriors: ${counter.toFixed(2)}`;
+  }
+});
+
+app.append(goblinButton);
+
+let skeletons = 0;
+const skeletonButton = document.createElement("button");
+skeletonButton.innerHTML = `<font size = "+2">💀</font><br>Skeletons: ${skeletons}`;
+skeletonButton.disabled = true;
+
+skeletonButton.addEventListener("click", () => {
+  skeletons++;
+  growthRate += 50;
+  if (counter >= 1000) {
+    counter -= 1000;
+    log.innerHTML = `Number of warriors: ${counter.toFixed(2)}`;
+  }
+});
+
+app.append(skeletonButton);
+
 let lastMillis = 0;
 
 const log = document.createElement("p");
 log.innerHTML = `No Warriors!`;
 app.append(log);
+
+let rate = document.createElement("div");
+app.append(rate);
 
 function tick(millis: number) {
   const delta = millis - lastMillis;
@@ -49,7 +89,17 @@ function tick(millis: number) {
   if (counter >= 10) {
     honorGuardButton.disabled = false;
   } else honorGuardButton.disabled = true;
+  if (counter >= 100) {
+    goblinButton.disabled = false;
+  } else goblinButton.disabled = true;
+  if (counter >= 1000) {
+    skeletonButton.disabled = false;
+  } else skeletonButton.disabled = true;
   counter += (growthRate * delta) / 1000;
+  goblinButton.innerHTML = `<font size = "+2">👺</font><br>Goblins: ${goblins}`;
+  honorGuardButton.innerHTML = `<font size = "+2">👹</font><br>Gremlins: ${gremlins}`;
+  skeletonButton.innerHTML = `<font size = "+2">💀</font><br>Skeletons: ${skeletons}`;
   log.innerHTML = `Number of warriors: ${counter.toFixed(2)}`;
+  rate.innerHTML = `${growthRate.toFixed(2)} warriors joining per second`;
   requestAnimationFrame(tick);
 }
